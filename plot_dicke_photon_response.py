fix dicke polariton formula

plot_dicke_photon_response.py
@@ -40,23 +40,30 @@ cbar = fig.colorbar(cm, pad = 0.0, aspect = 40)
 ax.axhline(W, c='k', ls=(0, (1, 5)), lw=1)
 ax.axvline(np.sqrt(W*wz/4),  c='k', ls=(0, (1, 5)), lw=1)
 
-# # ---------- two oscillator polaritons -------------
-# up_twoosc = []
-# lp_twoosc = []
-# for i, lam in enumerate(lam0s):
-#     pm, pp = poles.polaritons(wz, W, lam)
-#     up_twoosc.append(pp)
-#     lp_twoosc.append(pm)
+# ---------- two oscillator polaritons -------------
+up_twoosc = []
+lp_twoosc = []
+for i, lam in enumerate(lam0s):
+    if z == 0:
+        pm, pp = poles.polaritons(wz, W, lam)
+    elif z == 1:
+        pm, pp = poles.polaritons(np.sqrt(wz*(wz + 4*lam**2/(W*wz))),
+                                W,
+                                lam*(1 + 4*lam**2/(W*wz))**(-1/4)
+                                )
+        
+    up_twoosc.append(pp)
+    lp_twoosc.append(pm)
     
-# ax.plot(lam0s, up_twoosc, c='b', label="Poles Two Osc. model", lw=1)
-# ax.plot(lam0s, lp_twoosc, c='b', lw=1)
+ax.plot(lam0s, up_twoosc, c='b', label="Dicke polaritons with $P^2$ term", lw=1)
+ax.plot(lam0s, lp_twoosc, c='b', lw=1)
 
-# ax.legend()
-# # ---------- two oscillator polaritons -------------
+ax.legend()
+# ---------- two oscillator polaritons -------------
 
 ax.set_ylim(0, 2)
 ax.set_xlabel(r'$\lambda / \Omega$')
 ax.set_ylabel(r'$\omega / \Omega$')
 ax.set_title(r'$-{\rm Im}D(\omega) \Omega$')
 
-fig.savefig('plots/dicke_photon_response.jpeg', bbox_inches='tight', dpi=300)
+fig.savefig('plots/dicke_p2_photon_response.jpeg', bbox_inches='tight', dpi=300)

plot_ising_photon_response.py
@@ -13,9 +13,9 @@ plot.set_rcParams(size = (6, 5), lw = 2, fs = 14)
 fig, axes = plt.subplots(1, 1, constrained_layout=True)
 ax = axes
 
-W = 1.5
-J = 0.75
-B = 0.05
+W = 1
+J = 1
+B = 0.0
 
 lam0s = np.linspace(0.0001, 2.5, 100)
 ws = np.linspace(0, 3, 100)
@@ -45,7 +45,9 @@ ax.axhline(2*J, c='k', ls=(0, (1, 5)), lw=1)
 # up_twoosc = []
 # lp_twoosc = []
 # for i, lam in enumerate(lam0s):
-#     pm, pp = poles.polaritons(wz, W, lam)
+#     mx = ising.variational_mx(J, B, W, lam)
+    
+#     pm, pp = poles.polaritons(2*np.sqrt(J**2 + (B + 2*lam**2*mx/W)**2), W, 0.5*lam)
 #     up_twoosc.append(pp)
 #     lp_twoosc.append(pm)
     

poles.py
@@ -2,17 +2,18 @@ import numpy as np
 
 def polaritons(wz, wc, lam):
     def para(wz, wc, lam):
-        innerroot = np.sqrt(wz**4 + wc**4 - 2*wz**2*wc**2 + 16*lam**2*wz*wc)
-        return np.sqrt(0.5 * (wz**2 + wc**2 - innerroot)), np.sqrt(0.5 * (wz**2 + wc**2 + innerroot))
+        innerroot = np.sqrt((wz**2 - wc**2)**2 + 16*lam**2*wz*wc)
+        return (np.sqrt(0.5 * (wz**2 + wc**2 - innerroot)),
+                np.sqrt(0.5 * (wz**2 + wc**2 + innerroot))
+        )
     
     def ferro(wz, wc, lam):
         mu = wz * wc / (4 * lam**2)
-        g = lam * mu * np.sqrt(2 / (1 + mu))
-        wztilde = wz * (1 + mu) / (2 * mu)
-        eps = wz * (1 - mu) * (3 + mu) / (8 * mu * (1 + mu))
         
-        innerroot = np.sqrt(wztilde**4 + wc**4 - 2*wztilde**2*wc**2 + 16*g**2*wztilde*wc + 4*(eps**2*wztilde**2 + eps*wztilde**3 - wc**2*eps*wztilde))
-        return np.sqrt(0.5 * (wztilde**2 + wc**2 + 2*eps*wztilde - innerroot)), np.sqrt(0.5 * (wztilde**2 + wc**2 + 2*eps*wztilde + innerroot))
+        innerroot = np.sqrt(((wz/mu)**2 - wc**2)**2 + 4*wc**2*wz**2)
+        return (np.sqrt(0.5*((wz/mu)**2 + wc**2 - innerroot)),
+                np.sqrt(0.5*((wz/mu)**2 + wc**2 + innerroot))
+        )
     
     if 4 * lam**2 < wz * wc:
         # paramagnetic phase